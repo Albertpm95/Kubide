c337bb292c61a2f2afb8b8663badaf573ac1bfcb
Albert <albertpm95@outlook.com>
2023-03-23 15:54:05 +0100

    Created constants file

---
+++ src/app/constants.ts
@@ -0,0 +1,12 @@
+export class APIROUTES {
+  static HEROESLIST = 'v1/public/characters'
+}
+
+export class COMPONENTS {
+  static LIST = 'list'
+  static DETAILS = 'details'
+}
+
+export class MODULES {
+  static HERO = 'hero'
+}
