8bde5684253caefc761d72b0eed1b02a2191d17b
Albert <albertpm95@outlook.com>
2023-03-23 21:05:29 +0100

    Imported form related modules

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -6,6 +6,7 @@ import { ListComponent } from './list/list.component';
 import { DetailsComponent } from './details/details.component';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { ComponentsModule } from 'app/components/components.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -14,6 +15,8 @@ import { ComponentsModule } from 'app/components/components.module';
     DetailsComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     CommonModule,
     PagesRoutingModule,
     InfiniteScrollModule,
