be4ea24bca1b74b90940338f3eb05c82dfc7a5e8
Albert <albertpm95@outlook.com>
2023-03-23 15:56:10 +0100

    Added routing. Fixed import.

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PagesComponent } from './pages.component';
+import { COMPONENTS } from '@constants';
+import { DetailsComponent } from './details/details.component';
+import { ListComponent } from './list/list.component';
 
-const routes: Routes = [{ path: '', component: PagesComponent }];
+const routes: Routes = [
+  { path: COMPONENTS.LIST, component: ListComponent },
+  { path: COMPONENTS.DETAILS, component: DetailsComponent },
+  {
+    path: '',
+    redirectTo: COMPONENTS.LIST,
+    pathMatch: 'prefix'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PagesRoutingModule } from './pages-routing.module';
-import { PagesComponent } from './pages.component';
 import { ListComponent } from './list/list.component';
 import { DetailsComponent } from './details/details.component';
 
 
 @NgModule({
   declarations: [
-    PagesComponent,
     ListComponent,
     DetailsComponent
   ],
