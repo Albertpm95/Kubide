77a68363dd715cf0a490d6377d86095d1a073947
Albert <albertpm95@outlook.com>
2023-03-23 17:22:35 +0100

    Updated service to get and map a list of Characters/Heroes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { APIROUTES } from '@constants'
+import { Character } from '@models/character'
 import { environments } from 'environments/environments'
-import { Observable } from 'rxjs'
+import { Observable, of, tap, map } from 'rxjs'
 
 
 @Injectable({
@@ -12,8 +13,9 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  getHeroesList(): Observable<any[]> {
-    console.log('list')
-    return this.http.get<any[]>(environments.MARVELBASEENDPOINT + APIROUTES.HEROESLIST)
+  getHeroesList(): Observable<Character[]> {
+    return this.http.get<any>(environments.MARVELBASEENDPOINT + APIROUTES.HEROESLIST).pipe(
+      map(response => response?.data?.results as Character[]) // Mapped because the server response contained more info than I wanted.
+    );
   }
 }
