25a2e2a1bc2dc1bbb1654e1e3248863571d5465c
Albert <albertpm95@outlook.com>
2023-03-23 19:15:19 +0100

    Set box-sizing as border-box

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: '', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) }];
+const routes: Routes = [{ path: '', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) }, { path: 'components', loadChildren: () => import('./components/components.module').then(m => m.ComponentsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
