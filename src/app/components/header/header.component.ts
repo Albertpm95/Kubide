4d08f1bbb19959a51bbd2430ee1be46628e97c63
Albert <albertpm95@outlook.com>
2023-03-23 19:54:58 +0100

    Updated header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { COMPONENTS } from '@constants';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-
+  listUrl = '/' + COMPONENTS.LIST
 }
