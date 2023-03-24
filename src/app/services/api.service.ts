dc2d5b6e3431ed016499b08d5b72c7373c0cac3e
Albert <albertpm95@outlook.com>
2023-03-24 13:49:03 +0100

    Updated api service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,8 +1,7 @@
 import { HttpClient } from '@angular/common/http'
 import { Injectable } from '@angular/core'
 import { APIROUTES } from '@constants'
-import { Character } from '@models/character'
-import { CharacterDataContainer, CharacterDataWrapper } from '@models/metadata'
+import { DataContainer, DataWrapper } from '@models/metadata'
 
 import { environments } from 'environments/environments'
 import { map, Observable } from 'rxjs'
@@ -14,19 +13,27 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  getCharactersList(limit: number, offset: number): Observable<CharacterDataContainer> {
-    return this.http.get<CharacterDataWrapper>(environments.MARVELBASEENDPOINT + APIROUTES.CHARACTERS, { params: { limit: limit, offset: offset } }).pipe(
-      map(characterDataWrapper => characterDataWrapper?.data as CharacterDataContainer) // Mapped because the server response contained more info than I wanted.
+  getCharactersList(limit: number, offset: number): Observable<DataContainer> {
+    return this.http.get<DataWrapper>(environments.MARVELBASEENDPOINT + APIROUTES.CHARACTERS, { params: { limit: limit, offset: offset } }).pipe(
+      map(characterDataWrapper => characterDataWrapper?.data as DataContainer) // Mapped because the server response contained more info than I wanted.
     );
   }
 
-  getCharactersFilteredListByStartsWith(limit: number, offset: number, partialName: string): Observable<CharacterDataContainer> {
-    return this.http.get<CharacterDataWrapper>(environments.MARVELBASEENDPOINT + APIROUTES.CHARACTERS, { params: { limit: limit, offset: offset, nameStartsWith: partialName } }).pipe(
-      map(characterDataWrapper => characterDataWrapper?.data as CharacterDataContainer) // Mapped because the server response contained more info than I wanted.
+  getCharactersFilteredListByStartsWith(limit: number, offset: number, partialName: string): Observable<DataContainer> {
+    return this.http.get<DataWrapper>(environments.MARVELBASEENDPOINT + APIROUTES.CHARACTERS, { params: { limit: limit, offset: offset, nameStartsWith: partialName } }).pipe(
+      map(characterDataWrapper => characterDataWrapper?.data as DataContainer) // Mapped because the server response contained more info than I wanted.
     );
   }
 
-  getCharacterById(idCharacter: number): Observable<Character> {
-    return this.http.get<any>(environments.MARVELBASEENDPOINT + APIROUTES.CHARACTERS + { idCharacter })
+  getCharacterById(characterID: number): Observable<DataContainer> {
+    return this.http.get<DataWrapper>(environments.MARVELBASEENDPOINT + APIROUTES.CHARACTERS + '/' + characterID).pipe(
+      map(characterDataWrapper => characterDataWrapper?.data as DataContainer)
+    )
+  }
+
+  getCharacterComicsById(characterID: number): Observable<DataContainer> {
+    return this.http.get<DataWrapper>(environments.MARVELBASEENDPOINT + APIROUTES.CHARACTERS + '/' + characterID + '/comics').pipe(
+      map(characterDataWrapper => characterDataWrapper?.data as DataContainer)
+    )
   }
 }
